e05b01a4c18ed247c7caf60a4159715e49553027
Xinyu Li <xinyuli4@andrew.cmu.edu>
2026-04-08 18:56:32 -0400

    Add engine option; Fix Engine::query method return type

--- include/gpjson/engine.hpp
+++ include/gpjson/engine.hpp
@@ -1,16 +1,26 @@
 #pragma once
 
-#include "index/index_builder.hpp"
-#include "query/query.hpp"
+#include "gpjson/index/index_builder.hpp"
+#include "gpjson/query/query.hpp"
+
+#include <cstddef>
+#include <string>
+#include <vector>
+
 namespace gpjson {
+
 struct EngineOptions {
-  index::IndexBuilderType index_builder_type;
+  index::IndexBuilderType index_builder_type{
+      index::IndexBuilderType::UNCOMBINED};
+  size_t file_partition_size{0};
 };
 
 class Engine {
 public:
-  std::vector<query::QueryResult> query(std::string file_path,
-                                        std::vector<std::string> queries_src,
-                                        EngineOptions options);
-}
+  query::MaterializedBatchResult
+  query(const std::string &file_path,
+        const std::vector<std::string> &queries_src,
+        const EngineOptions &options) const;
+};
+
 } // namespace gpjson
