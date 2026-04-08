f2b4febb461ef8eededff55098e636c206e77c24
Xinyu Li <xinyuli4@andrew.cmu.edu>
2026-04-08 18:41:49 -0400

    Implement skeleton for file access subsystem
    Split file into partitions, where partitions are below certain size and
    ends with newline

--- include/gpjson/file/file_reader.hpp
+++ include/gpjson/file/file_reader.hpp
@@ -1,5 +1,26 @@
 #pragma once
 
+#include "gpjson/file/file.hpp"
+
+#include <cstddef>
+#include <string>
+#include <vector>
+
 namespace gpjson::file {
-class FileReader {};
+
+class FileReader {
+public:
+  explicit FileReader(std::string file_path);
+
+  void create_partitions(size_t partition_size_bytes);
+  const std::vector<PartitionView> &get_partition_views() const;
+  const FileMetadata &metadata() const;
+
+private:
+  std::string file_path_;
+  FileMetadata metadata_;
+  std::vector<std::byte> mapped_bytes_;
+  std::vector<PartitionView> partitions_;
+};
+
 } // namespace gpjson::file
