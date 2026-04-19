e374b4da393d3e30eb5eda576b5076144241bd97
Xinyu Li <xinyuli4@andrew.cmu.edu>
2026-04-19 13:53:08 -0400

    Add params

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,4 +1,5 @@
 #include "gpjson/engine.hpp"
+#include "gpjson/index/index_builder.hpp"
 #include "gpjson/log/log.hpp"
 
 #include <iostream>
@@ -9,13 +10,26 @@ int main() {
 
   LogInfo("The app is currently just a placeholder.");
 
-  gpjson::EngineOptions options{};
+  // Same parameters as the default ones in GpJSON Java codebase
+  gpjson::EngineOptions options{
+      .index_builder_options = gpjson::index::IndexBuilderOptions{
+          .index_builder_type = gpjson::index::IndexBuilderType::COMBINED,
+          .grid_size = 1024 * 16,
+          .block_size = 1024,
+          .reduction_grid_size = 32,
+          .reduction_block_size = 32,
+      }};
+
   gpjson::Engine engine{};
 
-  const std::string dataset_path = "./datasets/twitter_large_record.json";
+  // const std::string dataset_path =
+  //     "/afs/andrew.cmu.edu/usr13/xinyuli4/private/15418/project/codebase/"
+  //     "dataset/twitter_large_record.json";
+  const std::string dataset_path =
+      "/afs/andrew.cmu.edu/usr13/xinyuli4/private/15418/project/codebase/"
+      "dataset/twitter_small_records_remove.json";
   const std::vector<std::string> queries{
       "$.user.lang",
-      "$.user.lang[?(@ == \"en\")]",
   };
 
   auto batch_result = engine.query(dataset_path, queries, options);
@@ -23,7 +37,17 @@ int main() {
   std::cout << "Initialized gpjson-cpp engine\n";
   std::cout << "Dataset: " << dataset_path << '\n';
   std::cout << "Submitted " << queries.size() << " batched queries\n";
-  (void)batch_result;
+  // // std::cout << "Batch result stats: " << batch_result << '\n';
+  // for (const auto &result : batch_result.queries()) {
+  //   std::cout << "query: " << result.query_text() << "\n";
+  //   for (const auto &line : result.lines()) {
+  //     std::cout << "\t";
+  //     for (const auto &value : line.values()) {
+  //       std::cout << value.json_text() << ", ";
+  //     }
+  //   }
+  //   std::cout << "\n";
+  // }
 
   return 0;
 }
