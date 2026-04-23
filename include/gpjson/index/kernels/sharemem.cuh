a9dd58c9a0d334b7bef3ca944829017c208367cc
Xinyu Li <xinyuli4@andrew.cmu.edu>
2026-04-23 10:22:49 -0400

    Remove dead and garbage code

--- include/gpjson/index/kernels/sharemem.cuh
+++ include/gpjson/index/kernels/sharemem.cuh
@@ -31,9 +31,6 @@ __global__ void escape_carry_index(const char *file, int fileSize,
 __global__ void escape_index(const char *file, long fileSize,
                              char *escapeCarryIndex, long *escapeIndex);
 
-__global__ void escape_index_sharemem(const char *file, long fileSize,
-                                      long *escapeIndex);
-
 __global__ void quote_index(const char *file, int fileSize, long *escapeIndex,
                             long *quoteIndex, char *quoteCarryIndex);
 
--- src/gpjson/index/kernels/sharemem/uncombined/escape-carry-index.cu
+++ src/gpjson/index/kernels/sharemem/uncombined/escape-carry-index.cu
@@ -6,68 +6,6 @@
 #include <cstdio>
 namespace gpjson::index::kernels::sharemem {
 
-// __global__ void escape_carry_index(const char *file, int fileSize,
-//                                    char *escapeCarryIndex) {
-//   int index = blockIdx.x * blockDim.x + threadIdx.x;
-//   int stride = blockDim.x * gridDim.x;
-//
-//   int charsPerThread = (fileSize + stride - 1) / stride;
-//   int bitmapAlignedCharsPerThread = ((charsPerThread + 64 - 1) / 64) * 64;
-//   int start = index * bitmapAlignedCharsPerThread;
-//   int end = start + bitmapAlignedCharsPerThread;
-//
-//   char carry = 0;
-//
-//   for (int i = start; i < end && i < fileSize; i += 1) {
-//     if (file[i] == '\\') {
-//       carry = 1 ^ carry;
-//     } else {
-//       carry = 0;
-//     }
-//   }
-//
-//   escapeCarryIndex[index] = carry;
-// }
-
-// __global__ void escape_carry_index(const char *file, int fileSize,
-//                                    char *escapeCarryIndex) {
-//   constexpr int BYTES_PER_THREAD = 64;
-//   constexpr int CHUNK_SIZE =
-//       BYTES_PER_THREAD * 512; // assuming blockDim.x == 512
-//
-//   __shared__ char file_chunk[CHUNK_SIZE];
-//
-//   int tid = threadIdx.x;
-//   int block_start = blockIdx.x * CHUNK_SIZE;
-//
-//   // coalesced load into shared
-//   for (int j = tid; j < CHUNK_SIZE; j += blockDim.x) {
-//     int global_idx = block_start + j;
-//     file_chunk[j] = (global_idx < fileSize) ? file[global_idx] : 0;
-//   }
-//   __syncthreads();
-//
-//   int local_start = tid * BYTES_PER_THREAD;
-//   int local_end = local_start + BYTES_PER_THREAD;
-//
-//   char carry = 0;
-//
-//   for (int j = local_start; j < local_end && block_start + j < fileSize; ++j)
-//   {
-//     char cur_char = file_chunk[j];
-//     if (cur_char == '\\') {
-//       carry ^= 1;
-//     } else {
-//       carry = 0;
-//     }
-//   }
-//
-//   int index = blockIdx.x * blockDim.x + tid;
-//   if (block_start + local_start < fileSize) {
-//     escapeCarryIndex[index] = carry;
-//   }
-// }
-
 __global__ void escape_carry_index(const char *file, int fileSize,
                                    char *escapeCarryIndex) {
   constexpr int BYTES_PER_THREAD = 64;
