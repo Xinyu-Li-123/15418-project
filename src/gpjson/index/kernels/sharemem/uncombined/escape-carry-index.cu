3515df91253c2a9189aac3da9a12ac8f270bb1cd
Xinyu Li <xinyuli4@andrew.cmu.edu>
2026-04-23 15:19:52 -0400

    Further optimize escae carry index by pack bytes when read from global mem and write to shared memory
    This is very effective: execution of escape carry index on ghc goes from
    12.20 ms (UNCOMBINED) to 3.18 ms (SHAREMEM), almost 4x faster

--- src/gpjson/index/kernels/sharemem/uncombined/escape-carry-index.cu
+++ src/gpjson/index/kernels/sharemem/uncombined/escape-carry-index.cu
@@ -11,17 +11,59 @@ __global__ void escape_carry_index(const char *file, int fileSize,
   constexpr int BYTES_PER_THREAD = 64;
   constexpr int CHUNK_SIZE = 32768; // 512 * 64
 
-  __shared__ char file_chunk[CHUNK_SIZE];
+  __shared__ alignas(16) char file_chunk[CHUNK_SIZE];
 
   int tid = threadIdx.x;
   int block_start = blockIdx.x * CHUNK_SIZE;
 
-  // Coalesced global load into shared memory
-  for (int j = tid; j < CHUNK_SIZE; j += blockDim.x) {
-    int global_idx = block_start + j;
-    file_chunk[j] = (global_idx < fileSize) ? file[global_idx] : 0;
+  // Coalesced global load into shared memory, 16 bytes at a time to make
+
+  char *file_chunk_bytes = file_chunk;
+  // NOTE: uint4 is a cuda data type of 4 unsigned int (16 bytes), while
+  // uint64_t is a 64-bit / 8 bytes unsigned integer. The naming is a bit
+  // confusing. We use uint64_t instead of uint2 (also 8 bytes) later because
+  // uint2 can't do bitwise opeartions like right shift
+
+  // TODO: Why uint4 is slower than uint2?
+
+  // constexpr int packed_bytes_gmem = 16;
+  // static_assert(CHUNK_SIZE % packed_bytes_gmem == 0);
+  // uint4 *file_chunk_packed_gmem = reinterpret_cast<uint4
+  // *>(file_chunk_bytes); const uint4 *file_packed_gmem =
+  // reinterpret_cast<const uint4 *>(file);
+
+  constexpr int packed_bytes_gmem = 8;
+  static_assert(CHUNK_SIZE % packed_bytes_gmem == 0);
+  uint2 *file_chunk_packed_gmem = reinterpret_cast<uint2 *>(file_chunk_bytes);
+  const uint2 *file_packed_gmem = reinterpret_cast<const uint2 *>(file);
+
+  constexpr int packed_elems_per_block = CHUNK_SIZE / packed_bytes_gmem;
+
+  for (int p = tid; p < packed_elems_per_block; p += blockDim.x) {
+    int global_byte_idx = block_start + p * packed_bytes_gmem;
+
+    if (global_byte_idx + packed_bytes_gmem <= fileSize) {
+      file_chunk_packed_gmem[p] =
+          file_packed_gmem[global_byte_idx / packed_bytes_gmem];
+    } else {
+      // Tail handling for the last partial packed load in the file.
+      char *dst = &file_chunk_bytes[p * packed_bytes_gmem];
+
+#pragma unroll
+      for (int b = 0; b < packed_bytes_gmem; ++b) {
+        int idx = global_byte_idx + b;
+        dst[b] = (idx < fileSize) ? file[idx] : 0;
+      }
+    }
   }
+
   __syncthreads();
+
+  // file chunk load in packed byte from shared memory
+  const uint64_t *file_chunk_packed_smem =
+      reinterpret_cast<const uint64_t *>(file_chunk);
+
+  const int packed_bytes_smem = 8;
 
   int local_start = tid * BYTES_PER_THREAD;
   int local_end = local_start + BYTES_PER_THREAD;
@@ -47,21 +89,21 @@ __global__ void escape_carry_index(const char *file, int fileSize,
 
     // Load multiple bytes at a time from shared memory, both to avoid bank
     // conflict and to
-    const uint64_t *file_chunk_packed =
-        reinterpret_cast<const uint64_t *>(file_chunk);
+    // const uint64_t *file_chunk_packed =
+    //     reinterpret_cast<const uint64_t *>(file_chunk);
+    //
+    // const int packed_bytes = 8;
 
-    const int packed_bytes = 8;
-
-    int word_start = local_start / packed_bytes;
-    int word_end = local_end / packed_bytes;
+    int word_start = local_start / packed_bytes_smem;
+    int word_end = local_end / packed_bytes_smem;
 
     for (int w = word_start;
-         w < word_end && block_start + w * packed_bytes < fileSize; ++w) {
-      uint64_t word = file_chunk_packed[w];
+         w < word_end && block_start + w * packed_bytes_smem < fileSize; ++w) {
+      uint64_t word = file_chunk_packed_smem[w];
 
 #pragma unroll
-      for (int b = 0; b < packed_bytes; ++b) {
-        int global_idx = block_start + w * packed_bytes + b;
+      for (int b = 0; b < packed_bytes_smem; ++b) {
+        int global_idx = block_start + w * packed_bytes_smem + b;
         if (global_idx >= fileSize) {
           break;
         }
@@ -77,22 +119,22 @@ __global__ void escape_carry_index(const char *file, int fileSize,
     }
 
   } else {
-    const uint64_t *file_chunk_packed =
-        reinterpret_cast<const uint64_t *>(file_chunk);
+    // const uint64_t *file_chunk_packed =
+    //     reinterpret_cast<const uint64_t *>(file_chunk);
+    //
+    // const int packed_bytes = 8;
 
-    const int packed_bytes = 8;
-
-    int word_start = local_start / packed_bytes;
-    int word_end = local_end / packed_bytes;
+    int word_start = local_start / packed_bytes_smem;
+    int word_end = local_end / packed_bytes_smem;
 
     bool done = false;
 
     for (int w = word_end - 1; w >= word_start && !done; --w) {
-      uint64_t word = file_chunk_packed[w];
+      uint64_t word = file_chunk_packed_smem[w];
 
 #pragma unroll
-      for (int b = packed_bytes - 1; b >= 0; --b) {
-        int local_byte = (w - word_start) * packed_bytes + b;
+      for (int b = packed_bytes_smem - 1; b >= 0; --b) {
+        int local_byte = (w - word_start) * packed_bytes_smem + b;
         int global_idx = block_start + local_start + local_byte;
         if (global_idx >= fileSize) {
           continue;
